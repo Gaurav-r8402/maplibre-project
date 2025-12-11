26a286ef669947b8ede2252559c6ee9c07d1db3f
Gaurav R <gaurav_intern@latlong.com>
2025-12-11 14:45:10 +0530

    updated bg color

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -13,7 +13,7 @@ export default async function Page() {
     }
     const data= await getDashboardData("");
   return (
-    <div className="flex w-full h-screen overflow-hidden">
+    <div className="flex w-full h-screen overflow-hidden dark:bg-black">
       <Sidebar />
       <div className="flex flex-col w-full">
         <DashboardController 
